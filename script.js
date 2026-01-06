3ae607adca28939feb864c2dbdbb5fe4ec2024c1
kranthikumar728 <kakumanukranthikumar@gmail.com>
2026-01-06 10:47:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const changeBtn = document.getElementById("change_button");
+
+changeBtn.addEventListener('click', () => {
+	const blockId = document.getElementById('block_id').value
+	const color = document.getElementById('color_id').value
+	const blockEle = document.getElementById(blockId);
+	blockEle.style.backgroundColor = color;
+})
+
+const resetBtn = document.getElementById('reset');
+
+resetBtn.addEventListener('click', () => {
+	const blocks = document.querySelectorAll('.grid-item');
+	blocks.forEach(b => b.style.backgroundColor = 'transparent')
+})
